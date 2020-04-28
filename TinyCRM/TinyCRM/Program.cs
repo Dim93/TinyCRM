ebfa672a83b3351b12c59a41f1c2b4f920ae0cbd
DimitrisTsounis <dimitris_tsounis@windowslive.com>
2020-04-28 18:25:13 +0300

    Added a new program.cs

---
+++ TinyCRM/TinyCRM/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace TinyCRM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+/*
+ * using System;
+
+namespace String_length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please enter text: ");
+            Console.WriteLine(IsValidAfm("123456789"));
+            Console.WriteLine(IsValidAfm("123"));
+            Console.WriteLine(IsValidAfm("   123456789"));
+            Console.WriteLine(IsAdult(18));
+
+        }
+
+        public static bool IsValidAfm(string afm)
+        {
+            if (String.IsNullOrWhiteSpace(afm))
+            {
+                Console.WriteLine("Afm is Empty.");
+            }
+
+            afm = afm.Trim();
+
+            if (int.TryParse(afm, out int number) && afm.Length == 9)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsAdult(int age)
+        {
+            if (age >= 18 && age <= 100)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            // return age >= 18 && age <= 100;
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            if (!String.IsNullOrWhiteSpace(email))
+            {
+                email = email.Trim();
+
+                if (email.Contains("@") &&
+                    (email.EndsWith(".com") || email.EndsWith(".gr")))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    } 
+}
+
+ * 
+ * */
